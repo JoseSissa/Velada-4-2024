ef5cf9e0e20f1b6ae56be95f34cd56a26441b193
Jose <sissamendoza@outlook.com>
2024-07-22 09:04:03 -0500

    Removing unnecesary code and components

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,17 @@
-import animations from '@jose-sissa-3s/tailwind-animations'
+import animations from "@jose-sissa-3s/tailwind-animations";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				'primary': '(var(--color-primary))',
-				'secondary': '(var(--color-secondary))',
-				'twitch': '#6441a4'
-			}
-		},
-	},
-	plugins: [animations],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "(var(--color-primary))",
+                secondary: "(var(--color-secondary))",
+                twitch: "(var(--color-twitch))",
+                ice: "(var(--color-twitch-ice))",
+            },
+        },
+    },
+    plugins: [animations],
+};
