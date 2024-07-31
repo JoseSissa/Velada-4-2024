331ba010c2e533764691df79ae038bb59659e9ea
Jose <sissamendoza@outlook.com>
2024-07-31 15:40:15 -0500

    Updated info

---
+++ src/const/sponsors.ts
@@ -0,0 +1,53 @@
+interface Sponsor {
+    id: string;
+    name: string;
+    url: string;
+}
+
+export const SPONSORS: Sponsor[] = [
+    {
+        id: "vicio",
+        name: "Vicio",
+        url: "https://www.ganasdevicio.com/",
+    },
+    {
+        id: "revolut",
+        name: "Revolut",
+        url: "https://revolut.com/",
+    },
+    {
+        id: "el-pozo",
+        name: "El Pozo",
+        url: "https://elpozo.com/",
+    },
+    {
+        id: "grefusa",
+        name: "Grefusa",
+        url: "https://grefusa.com/",
+    },
+    {
+        id: "spotify",
+        name: "Spotify",
+        url: "https://spotify.com/",
+    },
+    {
+        id: "cerave",
+        name: "Cerave",
+        url: "https://cerave.es/",
+    },
+    {
+        id: "prime",
+        name: "Prime",
+        url: "https://drinkprime.com/",
+    },
+    {
+        id: "alsa",
+        name: "Alsa",
+        url: "https://alsa.es/",
+    },
+    {
+        id: "maxibon",
+        name: "Maxibon",
+        url: "https://maxibon.es/",
+    },
+] as const;
