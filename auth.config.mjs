9cde6db73563fd59032d540a2fd7c46f18c50584
Jose Sissa <sissamendoza@outlook.com>
2024-09-27 10:19:09 -0500

    Fixing prod in vercel

--- auth.config.mjs
+++ auth.config.mjs
@@ -1,14 +1,14 @@
-import GitHub from "@auth/core/providers/github";
-import GitHub from "@auth/core/providers/twitch";
+// import GitHub from "@auth/core/providers/github";
+import Twitch from "@auth/core/providers/twitch";
 
 import { defineConfig } from "auth-astro";
 
 export default defineConfig({
     providers: [
-        GitHub({
-            clientId: import.meta.env.GITHUB_CLIENT_ID,
-            clientSecret: import.meta.env.GITHUB_CLIENT_SECRET,
-        }),
+        // GitHub({
+        //     clientId: import.meta.env.GITHUB_CLIENT_ID,
+        //     clientSecret: import.meta.env.GITHUB_CLIENT_SECRET,
+        // }),
         Twitch({
             clientId: import.meta.env.TWITCH_CLIENT_ID,
             clientSecret: import.meta.env.TWITCH_CLIENT_SECRET,
