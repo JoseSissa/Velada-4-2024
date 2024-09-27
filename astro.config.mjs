d9a19c25518ad62d538bb5fe057b34042bc73ece
Jose Sissa <sissamendoza@outlook.com>
2024-09-27 09:06:08 -0500

    Add astro auth and upgrade some dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,13 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+import auth from "auth-astro";
+
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind()]
+  integrations: [tailwind(), auth()],
+  adapter: vercel()
 });
