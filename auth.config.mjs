6c8fca546e45e856d54fe5b1ddd13959b2839834
Jose Sissa <sissamendoza@outlook.com>
2024-09-27 11:46:50 -0500

    Sign in with astro auth, twitch and github

--- auth.config.mjs
+++ auth.config.mjs
@@ -1,4 +1,5 @@
 import Twitch from "@auth/core/providers/twitch";
+import GitHub from "@auth/core/providers/github";
 
 import { defineConfig } from "auth-astro";
 
@@ -7,6 +8,10 @@ export default defineConfig({
         Twitch({
             clientId: import.meta.env.TWITCH_CLIENT_ID,
             clientSecret: import.meta.env.TWITCH_CLIENT_SECRET,
+        }),
+        GitHub({
+            clientId: import.meta.env.GITHUB_CLIENT_ID_DEV,
+            clientSecret: import.meta.env.GITHUB_CLIENT_SECRET_DEV,
         }),
     ],
 });
