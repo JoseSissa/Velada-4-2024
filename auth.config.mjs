1bb7f3fd8e703a4e8c5754f2ae7a04c9e46a611b
Jose Sissa <sissamendoza@outlook.com>
2024-09-27 09:51:17 -0500

    ENV for twitch auth

---
+++ auth.config.mjs
@@ -0,0 +1,17 @@
+import GitHub from "@auth/core/providers/github";
+import GitHub from "@auth/core/providers/twitch";
+
+import { defineConfig } from "auth-astro";
+
+export default defineConfig({
+    providers: [
+        GitHub({
+            clientId: import.meta.env.GITHUB_CLIENT_ID,
+            clientSecret: import.meta.env.GITHUB_CLIENT_SECRET,
+        }),
+        Twitch({
+            clientId: import.meta.env.TWITCH_CLIENT_ID,
+            clientSecret: import.meta.env.TWITCH_CLIENT_SECRET,
+        }),
+    ],
+});
