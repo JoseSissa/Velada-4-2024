7c81a3cba7f0a10a095c798d5fdbc82039eddedb
Jose Sissa <sissamendoza@outlook.com>
2024-09-27 10:25:25 -0500

    Fix vercel prod

--- auth.config.mjs
+++ auth.config.mjs
@@ -1,14 +1,9 @@
-// import GitHub from "@auth/core/providers/github";
 import Twitch from "@auth/core/providers/twitch";
 
 import { defineConfig } from "auth-astro";
 
 export default defineConfig({
     providers: [
-        // GitHub({
-        //     clientId: import.meta.env.GITHUB_CLIENT_ID,
-        //     clientSecret: import.meta.env.GITHUB_CLIENT_SECRET,
-        // }),
         Twitch({
             clientId: import.meta.env.TWITCH_CLIENT_ID,
             clientSecret: import.meta.env.TWITCH_CLIENT_SECRET,
